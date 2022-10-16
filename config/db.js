05acaf2865fe6a8247c4749f24c502bcb2a3b345
Manoel Souza <48496686+manoelsouzadev@users.noreply.github.com>
2022-10-15 21:14:00 -0300

    Delete db.js

--- config/db.js
+++
@@ -1,5 +0,0 @@
-if(process.env.NODE_ENV == "production"){
-	module.exports = { mongoURI: 'mongodb+srv://manoel:mongodbnosql@mongodb-akelj.mongodb.net/test?retryWrites=true&w=majority' }
-}else{
-  module.exports = { mongoURI: 'mongodb+srv://manoel:mongodbnosql@mongodb-akelj.mongodb.net/test?retryWrites=true&w=majority' }
-}
